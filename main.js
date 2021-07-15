60e59e0545ec5c9f155d8a16e92e171878574aa2
Jacob Klimenko <droidedjake@gmail.com>
2021-07-14 23:02:20 -0400

    commit tree d3cbfb17bd2864606d3f28f82b64d5ada9079c2f

--- main.js
+++ main.js
@@ -4,7 +4,46 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+// collect all heart icons
+// iterate over icons and add
+// event listener for click event
+// that invokes mimicServerCall
 
+const hearts = document.getElementsByClassName("like-glyph");
+
+for (const heart of hearts) {
+
+  heart.addEventListener("click", function(e) {
+    if (heart.classList.contains("activated-heart")) {
+      // if heart full
+
+      // make heart empty
+      heart.classList.remove("activated-heart");
+      heart.innerHTML = EMPTY_HEART;
+
+    } else {
+      // if heart empty
+
+      mimicServerCall()
+        .then(function(response) {
+          // make heart full
+          heart.innerHTML = FULL_HEART;
+          heart.classList.add("activated-heart");
+        })
+        .catch(function(error) {
+          // if server call fails
+          // add & display error msg
+          let errorMessage = document.getElementById("modal-message");
+          errorMessage.innerHTML = error;
+
+          const errorModal = document.getElementById("modal");
+          errorModal.classList.remove("hidden");
+          setTimeout(() => errorModal.classList.add("hidden"), 5000);
+        });
+
+    };
+  });
+};
 
 
 //------------------------------------------------------------------------------
